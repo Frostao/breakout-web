68cc630bdc3024c46583a363268ccd7a32751b4d
jmcdoug <jmcdoug@purdue.edu>
2015-09-12 02:59:42 -0400

    some stuff for list

--- app/js/src/list/controller.js
+++ app/js/src/list/controller.js
@@ -1,5 +1,10 @@
-var app = angular.module('breakout.list', []);
+var app = angular.module('breakout.list', ['breakoutApp']);
 
-app.controller('listController', ['$scope', function($scope) {
- 
+app.controller('listController', ['$scope', 'eventService', function($scope, eventService) {
+ 	$scope.getEvents = function() {
+ 		//Doesn't work because the query doesn't return an array of events
+ 		//And even if it did i forgot to populate the fields of the objects
+ 		console.log(eventService.getEvents());
+ 		return eventService.getEvents();
+ 	}
 }]);
