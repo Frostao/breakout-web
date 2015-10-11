74bd10cbef48ffe0b3dae1d67430b15a413e73dc
Kartik Yadav <yadav11@purdue.edu>
2015-10-11 09:57:11 -0400

    Design changes

--- app/app.js
+++ app/app.js
@@ -32,6 +32,48 @@ app.config(function($routeProvider) {
         });
 });
 
+app.run(['$rootScope', function($scope) {
+  $scope.currentUser = Parse.User.current();
+
+ 
+  $scope.signUp = function(form) {
+    var user = new Parse.User();
+    user.set("email", form.email);
+    user.set("username", form.username);
+    user.set("password", form.password);
+    user.set("category", form.category);
+ 
+    user.signUp(null, {
+      success: function(user) {
+        $scope.currentUser = user;
+        alert("Congratulations!")
+        $scope.$apply(); // Notify AngularJS to sync currentUser
+      },
+      error: function(user, error) {
+        alert("Unable to sign up:  " + error.code + " " + error.message);
+      }
+    });    
+  }
+ 
+  $scope.logOut = function(form) {
+    Parse.User.logOut();
+    $scope.currentUser = null;
+  }
+
+  $scope.logIn = function(form) {
+    Parse.User.logIn(form.username, form.password, {
+      success: function(user) {
+        $scope.currentUser = user;
+        $scope.$apply();
+      },
+      error: function(user, error) {
+        alert("Unable to log in: " + error.code + " " + error.message);
+      }
+    });
+  }
+
+}]);
+
 app.service('eventService', function($q) {
      var Event = Parse.Object.extend("Event");
 
